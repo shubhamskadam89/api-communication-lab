ee1dc703e5e0abc36ee20ba25ac26d721b79b98f
Shubham Kadam <shubhamskadam89@gmail.com>
2026-07-17 13:34:13 +0530

    Implement repository-service database migration and entity layout

--- repository-service/build.gradle.kts
+++ repository-service/build.gradle.kts
@@ -5,12 +5,18 @@ plugins {
 
 dependencies {
     implementation(project(":shared-proto"))
+    implementation(libs.spring.boot.starter.web)
     implementation(libs.spring.boot.starter.data.jpa)
     implementation(libs.spring.boot.starter.actuator)
     implementation(libs.grpc.server.spring.boot)
     implementation(libs.postgresql)
     implementation(libs.flyway.core)
     implementation(libs.flyway.database.postgresql)
+
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    testCompileOnly("org.projectlombok:lombok")
+    testAnnotationProcessor("org.projectlombok:lombok")
 
     testImplementation(libs.spring.boot.starter.test)
 }
