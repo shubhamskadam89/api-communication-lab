0e50ec5d8cc70822eb77700bffc74d0b64b7b7ec
Shubham Kadam <shubhamskadam89@gmail.com>
2026-07-17 13:52:12 +0530

    Configure Swagger OpenAPI documentation for repository-service

---
+++ repository-service/src/main/java/com/example/apilab/repository/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.example.apilab.repository.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Repository Service API")
+                        .version("1.0")
+                        .description("API documentation for the Repository Service"));
+    }
+}
